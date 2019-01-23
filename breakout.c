dd22ed1f321c53319c3868415210769078a2796d
manila <manila@users.noreply.github.com>
2019-01-22 23:19:04 -0700

    Added game_loop and ball bounce logic

--- breakout.c
+++ breakout.c
@@ -7,6 +7,57 @@
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 
+int SCREEN_HEIGHT = 3;
+int SCREEN_WIDTH = 5;
+
+int ball_dirx = 1;
+int ball_diry = 1;
+
+int ball_posx = 3;
+int ball_posy = 2;
+
+void draw_box(int x, int w, int width, int height)
+{
+
+}
+
+void draw_pixel(int x, int y, int color)
+{
+	
+}
+
+void game_loop(void)
+{
+	int delta;
+
+	while (1)
+	{
+		if (ball_posx > SCREEN_WIDTH)
+		{
+			ball_dirx = -1;
+		}
+		else if (ball_posx < 1)
+		{
+			ball_dirx = 1; 
+		}
+
+		if (ball_posy > SCREEN_HEIGHT)
+		{
+			ball_diry = -1;
+		} else if (ball_posy < 1)
+		{
+			ball_diry = 1;
+		}
+
+		ball_posx += ball_dirx;
+		ball_posy += ball_diry;
+
+
+		printf("ball direction x: %d, ball direction y: %d\n", ball_dirx, ball_diry);
+		sleep(1);
+	}
+}
+
 int main(int ac, char **av)
 {
 	int fbfd = 0;
@@ -46,6 +97,8 @@ int main(int ac, char **av)
 
 	munmap(fbp, screensize);
 	close(fbfd);
+
+	game_loop();
 
 	return (0);
 }
